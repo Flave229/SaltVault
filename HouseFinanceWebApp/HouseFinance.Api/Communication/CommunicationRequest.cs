74b9624f0949091bfba8198ac08159e7e7826e5c
Flave <david@flave.co.uk>
2016-09-25 22:34:22 +0100

    Moved all of the API endpoints into a communication service that will pick the correct action from the request type found in the URL

--- HouseFinanceWebApp/HouseFinance.Api/Communication/CommunicationRequest.cs
+++ HouseFinanceWebApp/HouseFinance.Api/Communication/CommunicationRequest.cs
@@ -2,8 +2,17 @@
 
 namespace HouseFinance.Api.Communication
 {
-    public class CommunicationRequest
+    public interface ICommunicationRequest
+    {
+        Guid AuthToken { get; set; }
+        string RequestType { get; set; }
+        string Id { get; set; }
+    }
+
+    public class CommunicationRequest : ICommunicationRequest
     {
         public Guid AuthToken { get; set; }
+        public string RequestType { get; set; }
+        public string Id { get; set; }
     }
 }
