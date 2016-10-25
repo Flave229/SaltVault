32728d68b4a4c369a90c4008eb49c3448d6de500
Flave <david@flave.co.uk>
2016-10-25 20:15:38 +0100

    Added a test to ensure that bills are correctly opened from a file with the correct amount returned

--- HouseFinanceWebApp/HouseFinance.Tests/Core/FileIO/GenericFileHelperTests.cs
+++ HouseFinanceWebApp/HouseFinance.Tests/Core/FileIO/GenericFileHelperTests.cs
@@ -1,19 +1,67 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.IO;
+using Newtonsoft.Json;
 using NUnit.Framework;
+using Services.FileIO;
+using Services.Models.FinanceModels;
 
 namespace HouseFinance.Tests.Core.FileIO
 {
     [TestFixture]
     class GivenAValidListOfBills
     {
-        [Test]
-        public void WhenOpeningTheBillListAllBillsAreReturned()
+        private string _filePath;
+        private List<Bill> _subject;
+
+        [SetUp]
+        public void WhenWhenOpeningTheBillList()
         {
-            
+            _filePath = AppDomain.CurrentDomain.BaseDirectory + @"\TestDirectory\billtest.txt";
+            var fileHelper = new GenericFileHelper(_filePath);
+
+            var billList = JsonConvert.SerializeObject(new List<Bill>
+            {
+                new Bill
+                {
+                    AmountOwed = 10,
+                    AmountPaid = new List<Guid>(),
+                    Due = new DateTime(2016, 10, 25),
+                    Name = "Test1",
+                    RecurringType = RecurringType.None,
+                    People = new List<Guid>()
+                },
+                new Bill
+                {
+                    AmountOwed = 229,
+                    AmountPaid = new List<Guid>
+                    {
+                        new Guid("2C362C71-29DD-4ABB-BC0F-409D85569B74")
+                    },
+                    Due = new DateTime(2016, 10, 25),
+                    Name = "Test2",
+                    RecurringType = RecurringType.None,
+                    People = new List<Guid>()
+                }
+            });
+
+            var directoryInfo = new FileInfo(_filePath);
+            directoryInfo.Directory?.Create();
+            File.WriteAllText(directoryInfo.FullName, billList);
+
+            _subject = fileHelper.Open<Bill>();
+        }
+
+        [Test]
+        public void ThenTheCorrectAmountOfBillsAreReturned()
+        {
+            Assert.That(_subject.Count, Is.EqualTo(2));
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            File.Delete(_filePath);
         }
     }
 }
