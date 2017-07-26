a4d146122c77d6da25986ab584eaf739af51e353
Flave <david@flave.co.uk>
2017-07-26 20:38:19 +0100

    Renaming some data files

--- DotNetCorePort/HouseFinance/src/HouseFinance.Core/FileManagement/FilePath.cs
+++ DotNetCorePort/HouseFinance/src/HouseFinance.Core/FileManagement/FilePath.cs
@@ -5,6 +5,6 @@
         public static string Bills = @"Data\Bills\bills.txt";
         public static string Payments = @"Data\Payments\payments.txt";
         public static string People = @"Data\People\people.txt";
-        public static string Shopping = @"Data\Shopping\ShoppingItems.txt";
+        public static string Shopping = @"Data\Shopping\shopping_items.txt";
     }
 }
