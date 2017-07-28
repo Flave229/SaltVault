62a3bdd00513018d9118b25267fe5b228ec12438
Flave <david@flave.co.uk>
2017-07-28 12:34:11 +0100

    Adding the API controller that will allow external services to connect to HouseFinance.

---
+++ DotNetCorePort/HouseFinance/src/HouseFinance.Core/Authentication/Authentication.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace HouseFinance.Core.Authentication
+{
+    public class Authentication
+    {
+        private static readonly List<Guid> AuthenticatedUsers = new List<Guid>
+        {
+            Guid.Parse("D2DB7539-634F-47C4-818D-59AD03C592E3")
+        };
+
+        public static bool CheckKey(Guid authKey)
+        {
+            return AuthenticatedUsers.Contains(authKey);
+        }
+    }
+}
---
+++ DotNetCorePort/HouseFinance/src/HouseFinance/Controllers/APIController.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using HouseFinance.Core.Authentication;
+using HouseFinance.Core.Bills;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HouseFinance.Controllers
+{
+    public class ApiController : Controller
+    {
+        [Route("Api/Bills")]
+        public GetBillsResponse GetBills(CommunicationRequest billsRequest)
+        {
+            var response = new GetBillsResponse();
+            try
+            {
+                if (Authentication.CheckKey(billsRequest.ApiKey) == false)
+                {
+                    response.AddError("The API Key was invalid");
+                    return response;
+                }
+
+                response.Bills = BillListBuilder.BuildBillList();
+                return response;
+            }
+            catch (Exception exception)
+            {
+                response.AddError($"An unexpected exception occured: {exception}");
+                return response;
+            }
+        }
+    }
+
+    public class GetBillsResponse : CommunicationResponse
+    {
+        public List<BillOverview> Bills { get; set; }
+    }
+
+    public class CommunicationResponse
+    {
+        public Error Error { get; set; }
+        public bool HasError { get; set; }
+        public List<string> Notifications { get; set; }
+
+        public void AddError(string message)
+        {
+            Error = new Error { Message = message };
+            HasError = true;
+        }
+    }
+
+    public class Error
+    {
+        public string Message { get; set; }
+    }
+
+    public class CommunicationRequest
+    {
+        public Guid ApiKey { get; set; }
+    }
+}
