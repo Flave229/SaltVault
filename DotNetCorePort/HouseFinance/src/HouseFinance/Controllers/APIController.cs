5e63749f6647879569f510d27097646d9dc65f5d
Flave <david@flave.co.uk>
2017-07-28 14:11:03 +0100

    Adding the ability to add bills and payments

--- DotNetCorePort/HouseFinance/src/HouseFinance/Controllers/APIController.cs
+++ DotNetCorePort/HouseFinance/src/HouseFinance/Controllers/APIController.cs
@@ -1,6 +1,9 @@
 ﻿using System;
+using System.Collections.Generic;
 using HouseFinance.Core.Authentication;
 using HouseFinance.Core.Bills;
+using HouseFinance.Core.Bills.Payments;
+using HouseFinance.Core.FileManagement;
 using HouseFinance.Core.Shopping;
 using HouseFinance.Models.API;
 using Microsoft.AspNetCore.Mvc;
@@ -56,6 +59,66 @@ namespace HouseFinance.Controllers
             return response;
         }
 
+        [HttpPost]
+        [Route("Api/Bills/Add")]
+        public CommunicationResponse AddBill([FromBody]Bill billRequest)
+        {
+            var response = new CommunicationResponse();
+            if (Authenticate(Request.Headers["Authorization"]) == false)
+            {
+                response.AddError("The API Key was invalid");
+                return response;
+            }
+
+            try
+            {
+                BillValidator.CheckIfValidBill(billRequest);
+                var genericFileHelper = new GenericFileHelper(FilePath.Bills);
+                genericFileHelper.AddOrUpdate<Bill>(billRequest);
+
+                response.Notifications = new List<string>
+                {
+                    $"The bill '{billRequest.Name}' has been added"
+                };
+            }
+            catch (Exception exception)
+            {
+                response.AddError($"An unexpected exception occured: {exception}");
+            }
+
+            return response;
+        }
+
+        [HttpPost]
+        [Route("Api/Bills/AddPayment")]
+        public CommunicationResponse AddPayment([FromBody]Payment paymentRequest)
+        {
+            var response = new CommunicationResponse();
+            if (Authenticate(Request.Headers["Authorization"]) == false)
+            {
+                response.AddError("The API Key was invalid");
+                return response;
+            }
+
+            try
+            {
+                PaymentValidator.CheckIfValidPayment(paymentRequest);
+                var genericFileHelper = new GenericFileHelper(FilePath.Payments);
+                genericFileHelper.AddOrUpdate<Payment>(paymentRequest);
+
+                response.Notifications = new List<string>
+                {
+                    "The payment has been added"
+                };
+            }
+            catch (Exception exception)
+            {
+                response.AddError($"An unexpected exception occured: {exception}");
+            }
+
+            return response;
+        }
+
         [HttpGet]
         [Route("Api/Shopping")]
         public GetShoppingResponse GetShoppingItems()
@@ -84,5 +147,9 @@ namespace HouseFinance.Controllers
             var apiKey = authorizationHeader.ToString().Replace("Token ", "");
             return Authentication.CheckKey(apiKey);
         }
+    }
+
+    public class GetShoppingDetailsResponse : CommunicationResponse
+    {
     }
 }
