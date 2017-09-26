fbb0e62a7d98ec84b83dba798e60306a4e3df122
Flave <david@flave.co.uk>
2017-09-27 00:19:46 +0100

    Fixing Tests

--- src/SaltVault.Tests/Bills/GivenABillListRequest/WhenTheRequestIsValid.cs
+++ src/SaltVault.Tests/Bills/GivenABillListRequest/WhenTheRequestIsValid.cs
@@ -39,7 +39,7 @@ namespace SaltVault.Tests.Bills.GivenABillListRequest
                 }
             };
             
-            var result = subject.GetBillListV2();
+            var result = subject.GetBillListV2(null);
 
             Assert.AreEqual(result.Bills.Count, 1);
         }
