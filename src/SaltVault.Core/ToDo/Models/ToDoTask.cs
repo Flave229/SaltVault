3a60290d8ecd36149c805d27713935754b90d543
David Smith <david@penkridge.co.uk>
2018-05-15 13:47:12 +0100

    Adding Title to the ToDoTask Model.

--- src/SaltVault.Core/ToDo/Models/ToDoTask.cs
+++ src/SaltVault.Core/ToDo/Models/ToDoTask.cs
@@ -7,7 +7,8 @@ namespace SaltVault.Core.ToDo.Models
     public class ToDoTask
     {
         public int Id { get; set; }
-        public DateTime Due { get; set; }
+        public string Title { get; set; }
+        public DateTime? Due { get; set; }
         public bool Complete { get; set; }
         public List<Person> PeopleForTask { get; set; }
     }
