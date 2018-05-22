961b4eeaa59600e9da8066508fb2e469b1170a50
David Smith <david@penkridge.co.uk>
2018-05-22 16:49:49 +0100

    Accidently commited the no build flag

--- src/SaltVault.WebApp/build.sh
+++ src/SaltVault.WebApp/build.sh
@@ -1,1 +1,1 @@
-dotnet restore && dotnet build ./src/SaltVault.WebApp/SaltVault.WebApp.csproj && dotnet test ./src/SaltVault.Tests/SaltVault.Tests.csproj -c Release -f netcoreapp2.0 --no-build -- MSTest.MaxCpuCount=1
+dotnet restore && dotnet build ./src/SaltVault.WebApp/SaltVault.WebApp.csproj && dotnet test ./src/SaltVault.Tests/SaltVault.Tests.csproj -c Release -f netcoreapp2.0 -- MSTest.MaxCpuCount=1
