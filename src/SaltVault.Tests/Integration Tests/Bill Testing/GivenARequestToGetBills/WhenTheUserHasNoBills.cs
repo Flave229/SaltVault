43077f45fc96a580de8ff304e0d2ad19c78d0bab
David Smith <david@penkridge.co.uk>
2018-05-22 02:58:55 +0100

    Ensuring that the list is empty when GET is called on the bills when the user has no bills. This is being added following some inconsistency in the endpoints between null and empty lists. Behaviour going forward should ALWAYS be empty lists

---
+++ src/SaltVault.Tests/Integration Tests/Bill Testing/GivenARequestToGetBills/WhenTheUserHasNoBills.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Net.Http;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Newtonsoft.Json;
+using SaltVault.Tests.TestingHelpers;
+using SaltVault.WebApp.Models.Bills;
+
+namespace SaltVault.Tests.Integration_Tests.Bill_Testing.GivenARequestToGetBills
+{
+    [TestClass]
+    public class WhenTheUserHasNoBills
+    {
+        private FakeTestingAccountHelper _fakeTestingAccountHelper;
+        private Guid _validSessionId;
+        private HttpClient _client;
+        private GetBillListResponse _getBillListResponse;
+
+        [TestInitialize]
+        public void Initialize()
+        {
+            _fakeTestingAccountHelper = new FakeTestingAccountHelper();
+            _validSessionId = _fakeTestingAccountHelper.GenerateValidFakeCredentials();
+            _client = EndpointHelper.CreateFakeServer();
+            EndpointHelper.SetAuthenticationToken(_validSessionId.ToString());
+
+            var response = _client.GetAsync("/Api/v2/Bills").Result;
+            string responseContent = response.Content.ReadAsStringAsync().Result;
+            _getBillListResponse = JsonConvert.DeserializeObject<GetBillListResponse>(responseContent);
+        }
+
+        [TestMethod]
+        public void ThenTheBillListIsEmpty()
+        {
+            Assert.AreEqual(_getBillListResponse.Bills.Count, 0);
+        }
+
+        [TestMethod]
+        public void ThenTheResponseContainsNoErrors()
+        {
+            Assert.IsFalse(_getBillListResponse.HasError);
+        }
+    }
+}
