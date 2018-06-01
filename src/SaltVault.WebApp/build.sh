8c8f50c74fb536cbf746d854c5cb3a07a24fa87d
David Smith <david@penkridge.co.uk>
2018-06-01 19:36:19 +0100

    Found out how to tell Travis how to look at the settings. I'm really hoping this makes it work.

--- src/SaltVault.WebApp/build.sh
+++ src/SaltVault.WebApp/build.sh
@@ -1,1 +1,1 @@
-dotnet restore && dotnet build ./src/SaltVault.WebApp/SaltVault.WebApp.csproj && dotnet test ./src/SaltVault.Tests/SaltVault.Tests.csproj -c Release -f netcoreapp2.0 -- MSTest.MaxCpuCount=1
+dotnet restore && dotnet build ./src/SaltVault.WebApp/SaltVault.WebApp.csproj && dotnet test ./src/SaltVault.Tests/SaltVault.Tests.csproj -c Release --settings test.runsettings -f netcoreapp2.0 -- MSTest.MaxCpuCount=1
